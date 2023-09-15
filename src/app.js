907815c10f44979e6aea8d0efaab62614b98965c
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2023-09-15 00:48:44 +0000

    Code an Excuse Generator in Javascript

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function random(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+function generatorExcuse() {
+  let result =
+    who[random(0, who.length)] +
+    " " +
+    action[random(0, action.length)] +
+    " " +
+    what[random(0, what.length)] +
+    " " +
+    when[random(0, when.length)];
+  const p = document.getElementById("excuse");
+  p.innerHTML = result;
+}
+
+generatorExcuse();
+
+document.addEventListener("onload", generatorExcuse);
